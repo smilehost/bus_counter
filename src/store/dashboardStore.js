3f6cef5569c16e204eb0c8fb772b889aab966205
twotee007 <rexllyz2003@gmail.com>
2025-12-03 17:03:28 +0700

    creat file

--- src/store/dashboardStore.js
+++ src/store/dashboardStore.js
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 
-export const useAuthStore = create((set) => ({
+export const dashboardStore = create((set) => ({
   user: { id: 1, name: "SmileXD", role: 1 }, // Default mock
   
   setUser: (user) => set({ user }),
